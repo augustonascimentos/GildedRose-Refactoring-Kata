92e1768449dd9dd7611838d56082a02240617447
augustonascimentos <augustonascimentos@hotmail.com>
2020-05-20 19:08:26 -0300

    improving tdd

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,50 +1,97 @@
 # -*- coding: utf-8 -*-
+def decrease_sell_by_day_value_by_one(item):
+    item.sell_in -= 1
+    return item
+
+
+def increase_quality_by_one(item):
+    item.quality += 1
+    return item
+
+
+def increase_quality_by_two(item):
+    item.quality += 2
+    if item.quality > 50:
+        item.quality = 50
+    return item
+
+
+def increase_quality_by_three(item):
+    item.quality += 3
+    if item.quality > 50:
+        item.quality = 50
+    return item
+
+
+def check_quality_quantity(item):
+    if 'Sulfuras' not in item.name:
+        if item.quality > 50:
+            raise Exception('Quality can not be over 50')
+        if item.quality < 0:
+            raise Exception('Quality can not be negative')
+
+
+def aged_brie_quality_increase_when_get_older(item):
+    if 'Aged Brie' in item.name:
+        decrease_sell_by_day_value_by_one(item)
+        increase_quality_by_one(item)
+
+
+def expired_sell_date(item):
+    if 'Sulfuras' not in item.name:
+        if item.sell_in < 0:
+            item.quality -= 2
+
+
+def backstage_passes(item):
+    if 'Backstage passes to a TAFKAL80ETC concert' in item.name:
+        if item.sell_in <= 10:
+            increase_quality_by_two(item)
+        if item.sell_in <= 5:
+            increase_quality_by_three(item)
+        if item.sell_in < 0:
+            item.quality = 0
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def check_quality_quantity(self):
-        quality = self.items[0].quality
-        name = self.items[0].name
-
-        if "Sulfuras" in name:
-            return self.items
-        if quality > 50:
-            raise Exception('Quality can not be over 50')
-        if quality < 0:
-            raise Exception('Quality can not be negative')
-
     def update_quality(self):
         for item in self.items:
-            self.check_quality_quantity()
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            check_quality_quantity(item)
+            expired_sell_date(item)
+            aged_brie_quality_increase_when_get_older(item)
+            backstage_passes(item)
+            a = 2
+            # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #     if item.quality > 0:
+            #         if item.name != "Sulfuras, Hand of Ragnaros":
+            #             item.quality = item.quality - 1
+            # else:
+            #     if item.quality < 50:
+            #         item.quality = item.quality + 1
+            #         if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.sell_in < 11:
+            #                 if item.quality < 50:
+            #                     item.quality = item.quality + 1
+            #             if item.sell_in < 6:
+            #                 if item.quality < 50:
+            #                     item.quality = item.quality + 1
+            # if item.name != "Sulfuras, Hand of Ragnaros":
+            #     item.sell_in = item.sell_in - 1
+            # if item.sell_in < 0:
+            #     if item.name != "Aged Brie":
+            #         if item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.quality > 0:
+            #                 if item.name != "Sulfuras, Hand of Ragnaros":
+            #                     item.quality = item.quality - 1
+            #         else:
+            #             item.quality = item.quality - item.quality
+            #     else:
+            #         if item.quality < 50:
+            #             item.quality = item.quality + 1
 
 
 class Item:
